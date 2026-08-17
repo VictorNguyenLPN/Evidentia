86cc2b5497791b3f96ad6461613472218f37cc87
VictorNguyenLPN <quanghuy71847@gmail.com>
2026-08-17 21:06:06 +0700

    chore(scripts): add get example data script

---
+++ scripts/data/get_example_data.py
@@ -0,0 +1,16 @@
+import json
+from src.config import DATA_DIR
+
+origin_data_path = DATA_DIR / "data.json"
+example_data_path = DATA_DIR / "data.json.example"
+
+with open(origin_data_path, "r") as file:
+    origin_data = json.load(file)
+
+example_data = origin_data[:10]
+
+with open(example_data_path, "w", encoding="utf-8") as file:
+    json.dump(example_data, file, ensure_ascii=False, indent=2)
+
+print(f"Origin: {len(origin_data)}")
+print(f"Save success example: {len(example_data)}")
