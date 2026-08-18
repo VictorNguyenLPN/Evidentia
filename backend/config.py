190dd2dfc4b1edad4037d2020a59dacb3a285022
VictorNguyenLPN <quanghuy71847@gmail.com>
2026-08-18 13:48:34 +0700

    chore: rename src folder to backend, update config find package to fit with new folder name

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+DATA_DIR = PROJECT_ROOT / "data"
+SCRIPTS_DIR = PROJECT_ROOT / "scripts"
+DOCS_DIR = PROJECT_ROOT / "docs"
+NOTEBOOK_DIR = PROJECT_ROOT / "notebook"
